fdddc9328249715416819ce31ad6b71b48172dce
Lisa Luo <llluo@ucsd.edu>
2020-02-15 11:16:39 -0800

    added link to apply

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src="/logo.gif" className="App-logo"/>
         <p>May 2-3, 2020 @ UCSD</p>
         <p>
+          <a href="https://www.tesc.events/register/hackxx2020" target="_blank">Apply now!</a><br />
           Check out our <a href="https://www.facebook.com/hackXXsd/" target="_blank">Facebook page</a> for updates!<br />
           Interested in sponsoring? Contact us at <a href="mailto:sponsor@tesc.ucsd.edu" target="_blank">sponsor@tesc.ucsd.edu</a>.
         </p>
